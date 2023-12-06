76dfc558ffc04ff0592a21f3da7ab2d0ea8e3e07
Vinícius Guimarães <vinijipa23@gmail.com>
2023-12-06 15:01:00 -0300

    feat: insertAllRatings, filterByRatingRange

--- Analise/filters/filterByRatingRange.py
+++ Analise/filters/filterByRatingRange.py
@@ -4,16 +4,15 @@ sys.path.append('..')
 from mongoConnection import getDBConnection
 
 db = getDBConnection()
-ratingCollection = db['ratingCollection']
+ratingCollection = db['ratingsCollection']
 
 def filterByRatingRange(first_rating, last_rating):
 
-    filter = {'rating': {'$gte': first_rating, "$lte": last_rating}}
-    
+    filter = {'averageRating': {'$gte': first_rating, "$lte": last_rating}}
+
     results = ratingCollection.find(filter)
     for result in results:
         print(f"Title id: {str(result['tconst'])}, Title rating: {str(result['averageRating'])}, Total votes: {str(result['numVotes'])}\n")    
-
 
 first_rating = "5.2"
 last_rating = "5.5"
