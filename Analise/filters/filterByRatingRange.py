12e70fc5e3ba216e72c77aa4d0f11ef3e21d9686
Vinícius Guimarães <vinijipa23@gmail.com>
2023-12-06 14:01:20 -0300

    feat: filter by rating range

---
+++ Analise/filters/filterByRatingRange.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('..') 
+
+from mongoConnection import getDBConnection
+
+db = getDBConnection()
+ratingCollection = db['ratingCollection']
+
+def filterByRatingRange(first_rating, last_rating):
+
+    filter = {'rating': {'$gte': first_rating, "$lte": last_rating}}
+    
+    results = ratingCollection.find(filter)
+    for result in results:
+        print(f"Title id: {str(result['tconst'])}, Title rating: {str(result['averageRating'])}, Total votes: {str(result['numVotes'])}\n")    
+
+
+first_rating = "5.2"
+last_rating = "5.5"
+filterByRatingRange(first_rating, last_rating)
