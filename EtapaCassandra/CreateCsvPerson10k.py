169b1dadd88d640efaade2a2292b78c640ad3d30
Vinícius Guimarães <vinijipa23@gmail.com>
2024-02-07 20:41:01 -0300

    Person table and upload

--- EtapaCassandra/CreateCsvPerson10k.py
+++ EtapaCassandra/CreateCsvPerson10k.py
@@ -1,3 +1,5 @@
 import pandas as pd
 
 names = pd.read_csv("../Analise/ImdbName.csv", nrows=10000)
+
+names.to_csv("ImdbName10k.csv", index=False)
---
+++ EtapaCassandra/test.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+titlesBasics = pd.read_csv("../Analise/ImdbTitleBasics.csv")
+titlesBasics.info()
