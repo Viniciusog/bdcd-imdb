a767b1f34e4fe6543624e254363d92106e458626
Vinícius Guimarães <vinijipa23@gmail.com>
2024-02-07 00:20:26 -0300

    Titles by directors

--- EtapaCassandra/CreateCsvTitleByDirector.py
+++ EtapaCassandra/CreateCsvTitleByDirector.py
@@ -1,14 +1,36 @@
 import pandas as pd
 
-#Leitura dos arquivos
-title_basic = pd.read_csv('https://raw.githubusercontent.com/Viniciusog/bdcd-imdb/main/Analise/ImdbTitleBasicsWithRating.csv',  error_bad_lines=False, nrows=10000)
+# Leitura dos arquivos
+title_basic = pd.read_csv('../Analise/ImdbTitleBasicsWithRating.csv', nrows=10000)
+title_crew = pd.read_csv('../Analise/ImdbTitleCrew.csv')
+title_crew.rename(columns={'tconst': 'tconstTitle'}, inplace=True)
 
-title_crew = pd.read_csv('https://raw.githubusercontent.com/Viniciusog/bdcd-imdb/main/Analise/ImdbTitleCrew.csv',  error_bad_lines=False, nrows=10000)
+# Merge dos Diretores
+title_by_director = pd.merge(title_basic, title_crew[['tconstTitle','directors']], left_on="tconst", right_on="tconstTitle", how='left')
 
-#Merge dos Diretores
-title_by_director = title_basic.merge(title_crew[['tconst', 'directors']], on='tconst', how='inner')
+# title_by_director = title_by_director.drop(columns=['tconstTitle'])
 
-title_by_director.rename(columns={'directors': 'nconst'}, inplace=True)
+# Lista para armazenar os dados das linhas formatadas
+df_formatado = pd.DataFrame(columns=title_basic.columns)
 
-#Criação do CSV
-title_by_director.to_csv('title_by_director.csv', index=False)
+# Loop sobre as linhas do DataFrame original
+for index, row in title_by_director.iterrows():
+    directors = row['directors'].split(',')  # Divide os diretores em uma lista
+    # Para cada diretor na lista de diretores
+    for director in directors:
+        # Cria um dicionário para armazenar os dados da linha formatada
+        nova_linha = row.copy()  # Converta a linha para um dicionário
+        # Define o diretor da nova linha como o diretor atual do loop
+        nova_linha['director'] = director.strip()  # Remove espaços em branco
+        # Adiciona o dicionário à lista de linhas formatadas
+        df_formatado = pd.concat([df_formatado, nova_linha.to_frame().T],ignore_index=True) 
+
+# Cria um DataFrame a partir da lista de linhas formatadas
+
+df_formatado = df_formatado.drop(columns=['tconstTitle'])
+
+# Exibe informações sobre o DataFrame resultante
+df_formatado.info()
+
+# Salva o DataFrame em um arquivo CSV
+df_formatado.to_csv("ImdbTitleBasicsDirectorRelation.csv", index=False)
